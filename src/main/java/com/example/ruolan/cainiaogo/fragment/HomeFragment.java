71ab1c04bec3f60ad8e06652f7eaa15edba788bc
wuyinlei <wuyinlei19931118@hotmail.com>
2015-11-11 17:38:34 +0800

    添加Slider图片游览，用到了github中的开源项目

--- src/main/java/com/example/ruolan/cainiaogo/fragment/HomeFragment.java
+++ src/main/java/com/example/ruolan/cainiaogo/fragment/HomeFragment.java
@@ -1,12 +1,19 @@
 package com.example.ruolan.cainiaogo.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.Indicators.PagerIndicator;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.example.ruolan.cainiaogo.R;
 
 /**
@@ -14,10 +21,92 @@ import com.example.ruolan.cainiaogo.R;
  */
 public class HomeFragment extends Fragment{
 
-    @Nullable
+    private static final String TAG = "HomeFragment";
+
+    //创建控件
+    private SliderLayout mSliderLayout;
+    private PagerIndicator mIndicator;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        //找到SliderLayout的实例通过findViewById
+        mSliderLayout = (SliderLayout) view.findViewById(R.id.slider);
+
+        mIndicator = (PagerIndicator) view.findViewById(R.id.custom_indicator);
+
+        initSlider();
+
         return view;
+    }
+
+    public void initSlider(){
+        //创建slider
+        TextSliderView textSliderView = new TextSliderView(this.getActivity());
+        textSliderView.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t2416/102/20949846/13425/a3027ebc/55e6d1b9Ne6fd6d8f.jpg");
+        textSliderView.description("新品推荐");
+        textSliderView.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+            @Override
+            public void onSliderClick(BaseSliderView slider) {
+                Toast.makeText(getActivity(),"新品推荐",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextSliderView textSliderView2 = new TextSliderView(this.getActivity());
+        textSliderView2.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1507/64/486775407/55927/d72d78cb/558d2fbaNb3c2f349.jpg");
+        textSliderView2.description("时尚男装");
+        textSliderView2.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+            @Override
+            public void onSliderClick(BaseSliderView slider) {
+                Toast.makeText(getActivity(), "时尚男装", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TextSliderView textSliderView3 = new TextSliderView(this.getActivity());
+        textSliderView3.image("http://m.360buyimg.com/mobilecms/s300x98_jfs/t1363/77/1381395719/60705/ce91ad5c/55dd271aN49efd216.jpg");
+        textSliderView3.description("家电秒杀");
+        textSliderView3.setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+            @Override
+            public void onSliderClick(BaseSliderView slider) {
+                Toast.makeText(getActivity(), "家电秒杀", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //添加进sliderShow()
+        mSliderLayout.addSlider(textSliderView);
+        mSliderLayout.addSlider(textSliderView2);
+        mSliderLayout.addSlider(textSliderView3);
+
+        //设置动画效果
+       // mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);   //系统默认的
+        //接下来使用自定义的
+        mSliderLayout.setCustomIndicator(mIndicator);
+        mSliderLayout.setCustomAnimation(new DescriptionAnimation());
+        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.RotateUp);
+        //3秒的时间
+        mSliderLayout.setDuration(3000);
+
+        //滑动图片的监听事件
+        mSliderLayout.addOnPageChangeListener(new ViewPagerEx.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.d(TAG,"onPageScrolled");
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG,"onPageSelected");
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.d(TAG,"onPageScrollStateChanged");
+            }
+        });
+
+
     }
 }
