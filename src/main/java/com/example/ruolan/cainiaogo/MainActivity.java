99008f9d0be5120d87d1cb9fd8a890c693c41f78
wuyinlei <wuyinlei19931118@hotmail.com>
2015-11-11 10:04:57 +0800

    完成简单的TabHost

--- src/main/java/com/example/ruolan/cainiaogo/MainActivity.java
+++ src/main/java/com/example/ruolan/cainiaogo/MainActivity.java
@@ -1,52 +1,44 @@
 package com.example.ruolan.cainiaogo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
+import com.example.ruolan.cainiaogo.fragment.HomeFragment;
+import com.example.ruolan.cainiaogo.weiget.FragmentTabHost;
+
+//FragmentTabHost的用法
+//第一步：Activity继承自FragmentActivity(AppCompatActivity这个也是继承自FragmentActivity的)
 public class MainActivity extends AppCompatActivity {
 
+
+    private FragmentTabHost mTabHost;      //这个使用的是已经写好的一个FragmentTabHost
+
+    private LayoutInflater mInflater;      
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        mInflater = LayoutInflater.from(this);
+        //找到tabhost
+        mTabHost = (FragmentTabHost) this.findViewById(android.R.id.tabhost);
+        //第二步：调用setup()方法
+        mTabHost.setup(this,getSupportFragmentManager(),R.id.realtabcontent);
+        //第三步：添加TabSpec
+        TabHost.TabSpec tabSpec = mTabHost.newTabSpec("home");
+        //在这里我们要加载布局，为了实现tabhost
+        View view = mInflater.inflate(R.layout.tab_indicator, null);
+        ImageView img = (ImageView) view.findViewById(R.id.icon_tab);
+        TextView text = (TextView) view.findViewById(R.id.txt_indicator);
+        img.setBackgroundResource(R.mipmap.icon_home);
+        text.setText("主页");
+        tabSpec.setIndicator(view);   //把布局加载到tabspec中
+        mTabHost.addTab(tabSpec, HomeFragment.class,null);    //然后实现它
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ src/main/java/com/example/ruolan/cainiaogo/fragment/HomeFragment.java
@@ -0,0 +1,9 @@
+package com.example.ruolan.cainiaogo.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ruolan on 2015/11/11.
+ */
+public class HomeFragment extends Fragment{
+}
