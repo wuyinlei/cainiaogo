6540112917f83e13aa1ad699d0661a401c7fb535
wuyinlei <wuyinlei19931118@hotmail.com>
2015-11-22 11:50:45 +0800

    集成ShareSDKKK,完成html页面的分享功能，当然所使用的分享sdk是第三方控件

--- src/main/java/com/example/ruolan/cainiaogo/activity/WareDetailActivity.java
+++ src/main/java/com/example/ruolan/cainiaogo/activity/WareDetailActivity.java
@@ -18,6 +18,8 @@ import com.example.ruolan.cainiaogo.weiget.CnToolbar;
 
 import java.io.Serializable;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import dmax.dialog.SpotsDialog;
 
 /**
@@ -47,12 +49,12 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
 
         //首先在进入到这的时候，拿到ware这个对象
         Serializable serializable = getIntent().getSerializableExtra(Contants.WARE);
-        if (serializable == null){
+        if (serializable == null) {
             this.finish();
         }
         mWares = (Wares) serializable;
 
-        mDialog = new SpotsDialog(this,"loading...");
+        mDialog = new SpotsDialog(this, "loading...");
         mDialog.show();
 
         mCartProvider = new CartProvider(this);
@@ -60,11 +62,55 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
         initWebView();
     }
 
-    private void initToolbar(){
+    private void initToolbar() {
         mToolbar.setRightButtonOnClickListener(this);
+        mToolbar.setRightButtonText("分享");
+        mToolbar.setRightButtonOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showShare();
+            }
+        });
     }
 
-    private void initWebView(){
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://www.baidu.com");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText(mWares.getName());
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+            oks.setImageUrl(mWares.getImgUrl());
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://www.baidu.com");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment(mWares.getName());
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://www.baidu.com");
+
+// 启动分享GUI
+        oks.show(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        ShareSDK.stopSDK();
+    }
+
+    private void initWebView() {
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setBlockNetworkImage(false);
@@ -77,7 +123,7 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
 
         //下面的这个是
         mWebView.setWebViewClient(new WareShow());
-        
+
     }
 
 
@@ -86,7 +132,7 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
         this.finish();
     }
 
-    class WareShow extends WebViewClient{
+    class WareShow extends WebViewClient {
 
         //下面的这个方法是查看页面是否加载完成，如果加载完成
         @Override
@@ -94,7 +140,7 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
             super.onPageFinished(view, url);
 
             //加载对话框消失
-            if (mDialog != null && mDialog.isShowing()){
+            if (mDialog != null && mDialog.isShowing()) {
                 mDialog.dismiss();
             }
             //调用加载详情
@@ -102,17 +148,17 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
         }
     }
 
-    class WebAppInterface{
+    class WebAppInterface {
 
         private Context mContext;
 
-        public WebAppInterface(Context context){
+        public WebAppInterface(Context context) {
             this.mContext = context;
         }
 
         //JS调用AS
         @JavascriptInterface
-        public void showDetail(){
+        public void showDetail() {
 
             runOnUiThread(new Runnable() {
                 @Override
@@ -124,14 +170,14 @@ public class WareDetailActivity extends AppCompatActivity implements View.OnClic
         }
 
         @JavascriptInterface
-        public void buy(long id){
+        public void buy(long id) {
 
             mCartProvider.put(mWares);
-            ToastUtils.show(mContext,"已经添加到购物车");
+            ToastUtils.show(mContext, "已经添加到购物车");
         }
 
         @JavascriptInterface
-        public void addFacorites(long id){
+        public void addFacorites(long id) {
 
         }
     }
